8b117a6d78f32555ddb5e0ff2e8ee6bb0abe9a0c
Sanskar Soni <sunscarsony@gmail.com>
2024-04-30 15:55:22 +0530

    Update GCD.java

--- ApnaCollegeAssignment/GCD.java
+++ ApnaCollegeAssignment/GCD.java
@@ -1,17 +1,36 @@
 import java.util.Scanner;
+
 public class GCD
 {
-    static int ans=1;
-    public static void greaterCommonDivisor(int num1,int num2)
+    static int gcd=1;
+    public static void greaterCommonDivisor(int num1, int num2)
     {
-
+        int small;
+        int large;
+        if(num1>num2){
+            small=num2;
+            large=num1;
+        }
+        else{
+            small=num1;
+            large=num2;
+        }
+        for(int i=1;i<=small;i++)
+        {
+            if(large%i==0 && small%i==0){
+                gcd=i;
+            }
+        }
+        return;
     }
     public static void main(String[] args)
     {
         Scanner input=new Scanner(System.in);
-        System.out.println("Enter the First Number:");
+        System.out.println("Enter the First Number :");
         int num1=input.nextInt();
+        System.out.println("Enter the Second Number :");
         int num2=input.nextInt();
         greaterCommonDivisor(num1, num2);
+        System.out.println("Result :"+ gcd);
     }
 }
