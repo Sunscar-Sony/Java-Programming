4bc014282d62241c2dd9a00f8fdb39e9fe4ff698
Sanskar Soni <sunscarsony@gmail.com>
2024-04-30 12:32:09 +0530

    Create GCD.java

---
+++ ApnaCollegeAssignment/GCD.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class GCD
+{
+    static int ans=1;
+    public static void greaterCommonDivisor(int num1,int num2)
+    {
+
+    }
+    public static void main(String[] args)
+    {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter the First Number:");
+        int num1=input.nextInt();
+        int num2=input.nextInt();
+        greaterCommonDivisor(num1, num2);
+    }
+}
