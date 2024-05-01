edb4470900635a2c67b811dd3cda7c3fd002bbe0
Sanskar Soni <sunscarsony@gmail.com>
2024-05-01 15:00:24 +0530

    Update MatrixArray.java

--- ApnaCollegeAssignment/MatrixArray.java
+++ ApnaCollegeAssignment/MatrixArray.java
@@ -3,6 +3,27 @@ public class MatrixArray
 {
     public static void main(String[] args)
     {
-        
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter the Number of Rows :");
+        int row=input.nextInt();
+        System.out.println("Enter the Number of Coloumn :");
+        int coloumn=input.nextInt();
+        int [][] userArray=new int[row][coloumn];
+        System.out.println("Enter the Elements in the Array :");
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<coloumn;j++)
+            {
+                userArray[i][j]=input.nextInt();
+            }
+        }
+        System.out.println("Your Input Array is :");
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<coloumn;j++)
+            {
+                System.out.print(userArray[i][j] + " ");
+            }
+        }
     }
 }
