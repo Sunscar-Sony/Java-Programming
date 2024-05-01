762bb265fa78180b9f17d65e59805acacc0fdd8c
Sanskar Soni <sunscarsony@gmail.com>
2024-05-01 14:25:21 +0530

    Update Search.java

--- ApnaCollegeAssignment/Search.java
+++ ApnaCollegeAssignment/Search.java
@@ -5,5 +5,26 @@ public class Search
         Scanner input=new Scanner(System.in);
         System.out.println("Enter the size of the Array");
         int size=input.nextInt();
+        int [] userArray = new int[size];
+        System.out.println("Enter the Elements of the Array :");
+        for(int i=0;i<size;i++){
+            userArray[i]=input.nextInt();
+        }
+        System.out.println("Enter the Data you want to search");
+        int searchItem=input.nextInt();
+        int count=size+1;
+        for(int i=0;i<size;i++){
+            if(userArray[i]==searchItem){
+                count=i;
+                break;
+            }
+        }
+        if(count<size){
+            System.out.println("Data Found...");
+            System.out.println("Index :"+ count);
+        }
+        else{
+            System.out.println("Data Not Found !");
+        }
     }
 }
