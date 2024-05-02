2d222df66eac1f5ea27c3db98ef371f4fd1ca383
Sanskar Soni <sunscarsony@gmail.com>
2024-05-02 10:14:55 +0530

    Update Palindrome.java

--- ApnaCollegeAssignment/Palindrome.java
+++ ApnaCollegeAssignment/Palindrome.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 // A number is palindrome if its reverse is same as the orignal Number
 public class Palindrome{
-    public static void main(String[] args)
-    {
-
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
     }
 }
