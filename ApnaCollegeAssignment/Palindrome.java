e19d74e48ea35058568f9337bfe0b667dff76226
Sanskar Soni <sunscarsony@gmail.com>
2024-05-02 10:14:14 +0530

    Create Palindrome.java

---
+++ ApnaCollegeAssignment/Palindrome.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+// A number is palindrome if its reverse is same as the orignal Number
+public class Palindrome{
+    public static void main(String[] args)
+    {
+
+    }
+}
