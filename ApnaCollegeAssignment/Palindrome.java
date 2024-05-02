d69546f7d8b852b91e69c810d84de82d07039d57
Sanskar Soni <sunscarsony@gmail.com>
2024-05-02 10:16:29 +0530

    Update Palindrome.java

--- ApnaCollegeAssignment/Palindrome.java
+++ ApnaCollegeAssignment/Palindrome.java
@@ -3,5 +3,9 @@ import java.util.Scanner;
 public class Palindrome{
     public static void main(String[] args){
         Scanner input=new Scanner(System.in);
+        System.out.println("Enter the Staring Number :");
+        int start=input.nextInt();
+        System.out.println("Enter the Ending Number :");
+        int end=input.nextInt();
     }
 }
