657e4f499ddcffcf6182307687dcfdd8a4fa1879
Sanskar Soni <sunscarsony@gmail.com>
2024-05-02 19:21:28 +0530

    Update DecimalToBinary.java

--- BitManipulation/DecimalToBinary.java
+++ BitManipulation/DecimalToBinary.java
@@ -5,6 +5,20 @@ public class DecimalToBinary
 {
     public static void main(String[] args)
     {
-
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter the Number :");
+        int num=input.nextInt();
+        StringBuilder str=new StringBuilder("");
+        while(num>0)
+        {
+            if(num%2==0){
+                str.append("0");
+            }
+            else{
+                str.append("1");
+            }
+            num=num/2;
+        }
+        System.out.println(str);
     }
 }
