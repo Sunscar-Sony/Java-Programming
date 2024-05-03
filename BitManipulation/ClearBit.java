8de9057a49bb824c23834a0bfc19b79640f11182
Sanskar Soni <sunscarsony@gmail.com>
2024-05-03 16:53:19 +0530

    Create ClearBit.java

---
+++ BitManipulation/ClearBit.java
@@ -0,0 +1,13 @@
+package BitManipulation;
+
+import java.util.Scanner;
+
+public class ClearBit
+{
+    public static void main(String[] args)
+    {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter the Number :");
+        int binaryNumber=input.nextInt();
+    }
+}
