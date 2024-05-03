3de9f0a8887c89fc6ce193fe3fbc4a8b2fa048ac
Sanskar Soni <sunscarsony@gmail.com>
2024-05-03 22:27:53 +0530

    Update ClearBit.java

--- BitManipulation/ClearBit.java
+++ BitManipulation/ClearBit.java
@@ -1,17 +1,16 @@
 package BitManipulation;
-
 import java.util.Scanner;
-
 public class ClearBit
 {
     public static void main(String[] args)
     {
         Scanner input=new Scanner(System.in);
         System.out.println("Enter the Number :");
-        int binaryNumber=input.nextInt();
-        System.out.println("Enter the Position :");
+        int num=input.nextInt();
+        System.out.println("Enter the position :");
         int pos=input.nextInt();
-        int bitMask=1<<pos -1;
-        
+        int bitMask=1<<pos-1;
+        bitMask=~(bitMask)&num;
+        System.out.println("Result :" + "\n" + bitMask);
     }
 }
