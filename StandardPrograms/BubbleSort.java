adf7cf930902b9ec98b0531ea0bf737fa96e4e09
Sanskar Soni <sunscarsony@gmail.com>
2024-05-04 15:25:25 +0530

    Update BubbleSort.java

--- StandardPrograms/BubbleSort.java
+++ StandardPrograms/BubbleSort.java
@@ -4,30 +4,24 @@ public class BubbleSort
     public static void main(String[] args)
     {
         Scanner input=new Scanner(System.in);
-        System.out.println("Enter the Lenght of the Array :");
+        System.out.println("Enter the lenght of array :");
         int n=input.nextInt();
-        int [] arr=new int [n];
-        System.out.println("Enter the Elements :");
-        //Taking the input in the Array
-        for(int i=0;i<n;i++)
-        {
+        int[] arr=new int[n];
+        System.out.println("Enter the elements :");
+        for(int i=0;i<n;i++){
             arr[i]=input.nextInt();
         }
-        // Logic fot Bubble Sort
-        for(int i=0;i<n-1;i++)
-        {
-            for(int j=i;j<n-1;j++)
-            {
-                if(arr[i]>arr[i+1]){
-                    int temp=arr[i];
-                    arr[i]=arr[i+1];
-                    arr[i+1]=temp;
+        //Bubble Sort Logic
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
                 }
             }
         }
-        //Printing the Elements in the Array
-        for(int i=0;i<n;i++)
-        {
+        for(int i=0;i<n;i++){
             System.out.print(arr[i]+" ");
         }
     }
