a0c13d0a835c6cd5ed3b091d5c1e68a95d7a8a99
Sanskar Soni <sunscarsony@gmail.com>
2024-05-04 15:32:54 +0530

    Update SelectionSort.java

--- StandardPrograms/SelectionSort.java
+++ StandardPrograms/SelectionSort.java
@@ -11,7 +11,21 @@ public class SelectionSort
         for(int i=0;i<n;i++){
             arr[i]=input.nextInt();
         }
-        
+        //Selection sort Logic
+        int least=0;
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=i;j<n;j++)
+            {
+                if(arr[j]<arr[least])
+                {
+                    least=j;
+                }
+            }
+            int temp=arr[i];
+            arr[i]=arr[least];
+            arr[least]=temp;
+        }
         for(int i=0;i<n;i++){
             System.out.print(arr[i]+" ");
         }
