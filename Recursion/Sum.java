e1f0adad5b2f6cb6d48a5e34de3ebadf7f85f031
Sanskar Soni <sunscarsony@gmail.com>
2024-05-05 17:42:26 +0530

    Create Sum.java

---
+++ Recursion/Sum.java
@@ -0,0 +1,11 @@
+package Recursion;
+import java.util.Scanner;
+public class Sum
+{
+    public static void main(String[] args)
+    {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter the Number:");
+        int num=input.nextInt();
+    }
+}
