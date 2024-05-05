2494f6f64e1660bc4d6d08d2c1e42e4b8e697636
Sanskar Soni <sunscarsony@gmail.com>
2024-05-05 17:46:08 +0530

    Update Sum.java

--- Recursion/Sum.java
+++ Recursion/Sum.java
@@ -2,10 +2,23 @@ package Recursion;
 import java.util.Scanner;
 public class Sum
 {
+    public static void printSum(int i,int num,int sum)
+    {
+        //Base Case
+        if(i==num){
+            sum+=num;
+            System.out.println("Result :"+"\n"+sum);
+            return;
+        }
+        //My Task to do
+        sum=sum+i;
+        printSum(i+1, num, sum);
+    }
     public static void main(String[] args)
     {
         Scanner input=new Scanner(System.in);
         System.out.println("Enter the Number:");
         int num=input.nextInt();
+        printSum(1, num, 0);
     }
 }
