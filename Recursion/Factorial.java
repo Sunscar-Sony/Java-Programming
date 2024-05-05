34682fe55b8090cd972633fa1f92ab5e8d7567df
Sanskar Soni <sunscarsony@gmail.com>
2024-05-05 17:59:56 +0530

    Update Factorial.java

--- Recursion/Factorial.java
+++ Recursion/Factorial.java
@@ -2,21 +2,22 @@ package Recursion;
 import java.util.Scanner;
 
 public class Factorial {
-    public static int fact(int num){
+    static int ans=1;
+    public static void fact(int num){
         //Base Case
-        if(num==0 || num==1){
-            return 1;
+        if(num==0)
+        {
+            System.out.println("Result :"+"\n"+ans);
+            return;
         }
         //My Task to do
-        int temp=fact(num-1);
-        int ans=num*temp;
-        return ans;
+        ans*=num;
+        fact(num-1);
     }
     public static void main(String[] args){
         Scanner input=new Scanner(System.in);
         System.out.println("Enter the Number :");
         int num=input.nextInt();
-        int factorial=fact(num);
-        System.out.println("Result :"+"\n"+factorial);
+        fact(num);
     }
 }
