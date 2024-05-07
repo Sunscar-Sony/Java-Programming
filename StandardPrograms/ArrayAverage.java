dcf3a37d9cbdea46c9f7386da1873805e1fd3866
Sanskar Soni <sunscarsony@gmail.com>
2024-05-07 12:27:23 +0530

    Update ArrayAverage.java

--- StandardPrograms/ArrayAverage.java
+++ StandardPrograms/ArrayAverage.java
@@ -4,5 +4,16 @@ public class ArrayAverage{
         Scanner input=new Scanner(System.in);
         System.out.println("Enter the Lenght of the Array:");
         int n=input.nextInt();
+        int[]arr=new int[n];
+        System.out.println("Enter the elements:");
+        for(int i=0;i<n;i++){
+            arr[i]=input.nextInt();
+        }
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+        }
+        int ans=sum/n;
+        System.out.println(ans);
     }
 }
