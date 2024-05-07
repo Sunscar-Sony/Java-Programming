fc732b533edfaf8dccedc7388ec7d3242aaeff2c
Sanskar Soni <sunscarsony@gmail.com>
2024-05-07 21:31:08 +0530

    Update DuplicateString.java

---
+++ Recursion/DuplicateString.java
@@ -0,0 +1,11 @@
+package Recursion;
+import java.util.Scanner;
+
+public class DuplicateString{
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter the String:");
+        String str=input.nextLine();
+        System.out.println("Result:");
+    }
+}
