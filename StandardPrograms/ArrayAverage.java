bba50995a8623bf626c5e97b79cf4497bd38f594
Sanskar Soni <sunscarsony@gmail.com>
2024-05-07 12:24:41 +0530

    Update ArrayAverage.java

--- StandardPrograms/ArrayAverage.java
+++ StandardPrograms/ArrayAverage.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class ArrayAverage{
     public static void main(String[] args){
         Scanner input=new Scanner(System.in);
-
+        System.out.println("Enter the Lenght of the Array:");
+        int n=input.nextInt();
     }
 }
