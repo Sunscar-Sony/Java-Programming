e48fa65f43226433b01a32dfcd6cd54b9349514e
Sanskar Soni <sunscarsony@gmail.com>
2024-05-07 21:12:52 +0530

    Create Subsequence.java

---
+++ Recursion/Subsequence.java
@@ -0,0 +1,15 @@
+package Recursion;
+import java.util.Scanner;
+
+public class Subsequence{
+    static String newString="";
+
+    public static void subsequence(String str,int index){
+        
+    }
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter the String:");
+        String str=input.nextLine();
+    }
+}
