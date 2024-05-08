fb3cf58e1e30e115d57d8971104cff7e5f52135d
Sanskar Soni <sunscarsony@gmail.com>
2024-05-08 21:23:37 +0530

    Update Subsequence.java

--- Recursion/Subsequence.java
+++ Recursion/Subsequence.java
@@ -3,7 +3,21 @@ package Recursion;
 import java.util.Scanner;
 
 public class Subsequence{
+    public static void subsequence(String str, int index,int newStr){
+        //Base Case
+        if(index==str.length()){
+            System.out.println(newStr);
+        }
+        //My Task to do
+        char current=str.charAt(index);
+        //if it is choose
+        subsequence(str, index+1, newStr+current);
+        //if not choosen
+        subsequence(str, index+1, newStr);
+    }
     public static void main(String[] args){
         Scanner input=new Scanner(System.in);
+        System.out.println("Enter the String :");
+        String str=input.nextLine();
     }
 }
