065e7544da28d5a027454cb8f400c1480d5188b5
Sanskar Soni <sunscarsony@gmail.com>
2024-05-09 19:11:20 +0530

    Update PermutationOfString.java

--- Recursion/PermutationOfString.java
+++ Recursion/PermutationOfString.java
@@ -1,7 +1,18 @@
 package Recursion;
 import java.util.Scanner;
 public class PermutationOfString{
+    public static void printpermutation(String str, String permutation){
+        //My task to do
+        for(int i=0;i<str.length();i++){
+            char current=str.charAt(i);
+            String newStr=str.substring(0,i)+str.substring(i+1);
+            printpermutation(newStr, permutation+current);
+        }
+    }
     public static void main(String[] args){
-        Scanner input=new Scanner(System.in)
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter the String :");
+        String str=input.nextLine();
+        System.out.println("Total Possible Permutations are:");
     }
 }
