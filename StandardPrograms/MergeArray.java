90a2535eb3d984ea81ef550f24d8d88d97c59f9b
Sanskar Soni <sunscarsony@gmail.com>
2024-05-09 10:32:44 +0530

    Update MergeArray.java

--- StandardPrograms/MergeArray.java
+++ StandardPrograms/MergeArray.java
@@ -6,15 +6,31 @@ public class MergeArray{
         int n1=input.nextInt();
         System.out.println("Enter the lenght of Array2 :");
         int n2=input.nextInt();
+        //Array Decleration.
         int[]arr=new int[n1];
         int[]brr=new int[n2];
+        //Taking elements in Array 1
         System.out.println("Enter elements in Array1:");
         for(int i=0;i<n1;i++){
             arr[i]=input.nextInt();
         }
+        //Taking elements in Array 2
         System.out.println("Enter elements in Array 2");
         for(int i=0;i<n2;i++){
             brr[i]=input.nextInt();
+        }
+        int[] crr=new int[n1+n2];
+        //merging the Array
+        for(int i=0;i<(n1+n2);i++){
+            if(i<n1){
+
+                crr[i]=arr[i];
+            }else{
+                crr[i]=brr[i-n1];
+            }
+        }
+        for(int i=0;i<(n1+n2);i++){
+            System.out.println(crr[i]);
         }
     }
 }
