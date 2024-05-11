5d321b26c52b9af7d2ce6a06505e83c5272f39d6
Sanskar Soni <sunscarsony@gmail.com>
2024-05-11 14:24:21 +0530

    Update Anagram.java

--- StandardPrograms/Anagram.java
+++ StandardPrograms/Anagram.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
 
 public class Anagram{
+    static int[] map=new int[26];
+    public static void checkAnagram(String str1, String str2){
+        if(str1.length()==str2.length()){
+
+        }
+    }
     public static void main(String[] args){
         Scanner input=new Scanner(System.in);
         System.out.println("Enter the First String");
