9bccb02dc6dee8c7d539f6e3e53bb980a5d921ad
Sanskar Soni <sunscarsony@gmail.com>
2024-05-11 14:21:30 +0530

    Update Anagram.java

--- StandardPrograms/Anagram.java
+++ StandardPrograms/Anagram.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 
 public class Anagram{
     public static void main(String[] args){
-        
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter the First String");
+        String str1=input.nextLine();
+        System.out.println("Enter the Second String:");
+        String str2=input.nextLine();
     }
 }
