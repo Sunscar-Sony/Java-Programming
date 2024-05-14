88ddb138dc8bccc4f1635e772773a3939a9a50e5
Sanskar Soni <sunscarsony@gmail.com>
2024-05-14 12:05:49 +0530

    Create LinkedListCollection.java

---
+++ LinkedList/LinkedListCollection.java
@@ -0,0 +1,11 @@
+package LinkedList;
+
+import java.util.Scanner;
+import java.util.LinkedList;
+
+public class LinkedListCollection{
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        LinkedList<String>list=new LinkedList<String>();
+    }
+}
