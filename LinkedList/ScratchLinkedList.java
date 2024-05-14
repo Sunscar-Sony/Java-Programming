4b683e777c47e3c2508968c365dc2e1996ca4843
Sanskar Soni <sunscarsony@gmail.com>
2024-05-14 18:35:04 +0530

    Update ScratchLinkedList.java

--- LinkedList/ScratchLinkedList.java
+++ LinkedList/ScratchLinkedList.java
@@ -2,6 +2,16 @@ package LinkedList;
 import java.util.Scanner;
 
 public class ScratchLinkedList{
+    node head;
+    public class node{
+        String data;
+        node next;
+        //Constructor
+        node(String data){
+            this.data=data;
+            this.next=null;
+        }
+    }
     public static void main(String[] args){
         ScratchLinkedList list = new ScratchLinkedList();
 
