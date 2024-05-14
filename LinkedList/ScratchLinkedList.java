43fa9ef15ac6a5cab31253067fac623d98e6fcd9
Sanskar Soni <sunscarsony@gmail.com>
2024-05-14 18:30:49 +0530

    Create ScratchLinkedList.java

---
+++ LinkedList/ScratchLinkedList.java
@@ -0,0 +1,9 @@
+package LinkedList;
+import java.util.Scanner;
+
+public class ScratchLinkedList{
+    public static void main(String[] args){
+        ScratchLinkedList list = new ScratchLinkedList();
+
+    }
+}
