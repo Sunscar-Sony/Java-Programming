d8a80561f31d5671d9ec96ab7dcce10a69ab222e
Sanskar Soni <sunscarsony@gmail.com>
2024-05-18 10:47:04 +0530

    Update List.java

---
+++ LinkedList/List.java
@@ -0,0 +1,9 @@
+package LinkedList;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class List{
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+    }
+}
