1fc60ec8a9c01ece7715bfd4c2888e118d7cf0d4
Sanskar Soni <sunscarsony@gmail.com>
2024-05-18 10:50:54 +0530

    Update List.java

--- LinkedList/List.java
+++ LinkedList/List.java
@@ -5,5 +5,13 @@ import java.util.ArrayList;
 public class List{
     public static void main(String[] args){
         Scanner input=new Scanner(System.in);
+        ArrayList<Integer> list=new ArrayList<>();
+        System.out.println("Current List: "+list);
+        System.out.println("Enter the no. of Element you wan to insert :");
+        int n=input.nextInt();
+        for(int i=0;i<n;i++){
+            list.add(input.nextInt());
+        }
+        System.out.println("Current List :"+list);
     }
 }
