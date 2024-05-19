d6948c6000493d2c471312e682b37d4a0b7020d2
Sanskar Soni <sunscarsony@gmail.com>
2024-05-19 18:01:51 +0530

    Create Duplicates.java

---
+++ StandardPrograms/Duplicates.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+import java.lang.StringBuilder;
+
+public class Duplicates{
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+    }
+}
+
