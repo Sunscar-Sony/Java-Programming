dfe125123d6ce4411e7e405cc9ad270ac03ec352
Sanskar Soni <sunscarsony@gmail.com>
2024-05-19 18:03:34 +0530

    Update Duplicates.java

--- StandardPrograms/Duplicates.java
+++ StandardPrograms/Duplicates.java
@@ -4,6 +4,10 @@ import java.lang.StringBuilder;
 public class Duplicates{
     public static void main(String[] args){
         Scanner input=new Scanner(System.in);
+        System.out.println("Enter the number :");
+        StringBuilder str=new StringBuilder(input.next());
+        StringBuilder result=new StringBuilder("");
+        
     }
 }
 
