55e2ec43bdd105eda000fbab7fa0de8f9e9328c3
Sanskar Soni <sunscarsony@gmail.com>
2024-05-20 17:52:07 +0530

    Update BinarySearch.java

--- StandardPrograms/BinarySearch.java
+++ StandardPrograms/BinarySearch.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class BinarySearch{
     public static void main(String[] args){
-        
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter the Length of Array :");
     }
 }
