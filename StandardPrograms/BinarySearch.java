32782ce7c09ed417dc73fd4d0be86f5570f70a59
Sanskar Soni <sunscarsony@gmail.com>
2024-05-20 17:54:23 +0530

    Update BinarySearch.java

--- StandardPrograms/BinarySearch.java
+++ StandardPrograms/BinarySearch.java
@@ -4,5 +4,11 @@ public class BinarySearch{
     public static void main(String[] args){
         Scanner input=new Scanner(System.in);
         System.out.println("Enter the Length of Array :");
+        int n=input.nextInt();
+        int[]arr=new int[n];
+        System.out.println("Enter the Elements in Sorted manner in the Array :");
+        for(int i=0;i<arr.length;i++){
+            arr[i]=input.nextInt();
+        }
     }
 }
