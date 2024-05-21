9c18459072f8c86f3fea294b0154777ac4940de4
Sanskar Soni <sunscarsony@gmail.com>
2024-05-21 15:49:58 +0530

    Create Temp.java

---
+++ StandardPrograms/Temp.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Temp{
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        StringBuilder str=new StringBuilder(input.next());
+        System.out.println(str.reverse());
+    }
+}
