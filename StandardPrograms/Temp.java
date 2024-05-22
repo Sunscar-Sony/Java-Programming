1dee17991e24001c5d8804249625610c07f67f40
Sanskar Soni <sunscarsony@gmail.com>
2024-05-22 15:43:31 +0530

    Update Temp.java

--- StandardPrograms/Temp.java
+++ StandardPrograms/Temp.java
@@ -2,7 +2,28 @@ import java.util.Scanner;
 public class Temp{
     public static void main(String[] args){
         Scanner input=new Scanner(System.in);
-        StringBuilder str=new StringBuilder(input.next());
-        System.out.println(str.reverse());
+        int size=input.nextInt();
+        int[] arr=new int[size];
+        for(int i=0;i<size;i++){
+            arr[i]=input.nextInt();
+        }
+        int greatest=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>greatest){
+                greatest=arr[i];
+            }
+        }
+        int[] frequency=new int[greatest+1];
+        for(int i=0;i<arr.length;i++){
+            frequency[arr[i]]+=1;
+        }
+        int temp=0;
+        for(int i=0;i<frequency.length;i++){
+            if(frequency[i]>temp){
+                temp=frequency[i];
+            }
+        }
+        int result=size-temp;
+        System.out.println(result);
     }
 }
