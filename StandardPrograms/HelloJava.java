929c8970e4c7d60a6181289780d12c8b856d645d
Sanskar Soni <sunscarsony@gmail.com>
2024-05-22 15:54:05 +0530

    Update HelloJava.java

--- StandardPrograms/HelloJava.java
+++ StandardPrograms/HelloJava.java
@@ -2,6 +2,9 @@ public class HelloJava
 {
     public static void main(String[] args)
     {
+        int n=110;
         System.out.println("Hello World!");
+        System.out.println("Hello System");
+        System.out.println(n);
     }
 }
