12349fe5646647192a2aea2fcc318bbbed005a00
Sanskar Soni <sunscarsony@gmail.com>
2024-05-23 17:43:47 +0530

    Update Subtraction.java

--- StandardPrograms/Subtraction.java
+++ StandardPrograms/Subtraction.java
@@ -5,5 +5,10 @@ public class Subtraction{
         Scanner input=new Scanner(System.in);
         System.out.println("Enter the lenght of First Matrix");
         int n=input.nextInt();
+        int[] arr=new int[n];
+        System.out.println("Enter the Elements in the Array :");
+        for(int i=0;i<n;i++){
+            arr[i]=input.nextInt();
+        }
     }
 }
