7b34aee086cabd936d8458ec2ebfacc29d433ec8
Sanskar Soni <sunscarsony@gmail.com>
2024-05-24 14:10:55 +0530

    Update Divisor.java

--- StandardPrograms/Divisor.java
+++ StandardPrograms/Divisor.java
@@ -4,5 +4,10 @@ public class Divisor{
         Scanner input=new Scanner(System.in);
         System.out.println("Enter the size of the Array :");
         int size=input.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter the Elements :");
+        for(int i=0;i<size;i++){
+            arr[i]=input.nextInt();
+        }
     }
 }
