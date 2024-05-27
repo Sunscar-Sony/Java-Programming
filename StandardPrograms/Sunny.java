f77a48808606759275e4cf8010c172ea145f80c3
Sanskar Soni <sunscarsony@gmail.com>
2024-05-27 18:37:56 +0530

    Update Sunny.java

--- StandardPrograms/Sunny.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class Sunny{
-    public static void main(String[] args){
-        Scanner input=new Scanner(System.in);
-        System.out.println("Enter the number :");
-        int num=input.nextInt();
-    }
-}
