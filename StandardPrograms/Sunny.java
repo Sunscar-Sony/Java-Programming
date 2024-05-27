3d2562b14e7489a44d4c51f021c6d0677b3278ff
Sanskar Soni <sunscarsony@gmail.com>
2024-05-27 19:04:28 +0530

    Update Sunny.java

--- StandardPrograms/Sunny.java
+++ StandardPrograms/Sunny.java
@@ -2,5 +2,14 @@ import java.util.Scanner;
 public class Sunny{
     public static void main(String[] args){
         Scanner input=new Scanner(System.in);
+        System.out.println("Enter the Number :");
+        int num=input.nextInt();
+        int temp=num+1;
+        int x=(int)Math.sqrt(temp);
+        if(x*x==temp){
+            System.out.println("The Number is Sunny...");
+        }else{
+            System.out.println("Not Sunny");
+        }
     }
 }
