0b12a94e5f6d8c27010e2800ba0763e06a6dcfba
Sanskar Soni <sunscarsony@gmail.com>
2024-05-28 11:56:45 +0530

    Update recursion.java

--- Recursion/recursion.java
+++ Recursion/recursion.java
@@ -3,6 +3,6 @@ import java.util.Scanner;
 public class recursion{
     public static void main(String[] args){
         Scanner input=new Scanner(System.in);
-        System.out.
+        System.out.println("");
     }
 }
