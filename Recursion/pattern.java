ccad55464997dbd3d9ea507d1b912fe02834fd04
Sanskar Soni <sunscarsony@gmail.com>
2024-05-29 19:18:23 +0530

    Update pattern.java

--- Recursion/pattern.java
+++ Recursion/pattern.java
@@ -6,7 +6,9 @@ class pattern{
         Scanner input=new Scanner(System.in);
         int n=input.nextInt();
         for(int i=1;i<=n;i++){
-            
+            for(int j=1;j<=n;j++){
+
+            }
         }
     }
 }
