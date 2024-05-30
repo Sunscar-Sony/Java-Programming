77b1c1f0d369b2778d5b778f8a11bd3a1cf19873
Sanskar Soni <166742286+Sunscar-Sony@users.noreply.github.com>
2024-05-30 20:35:51 +0530

    Update BinaryToDecimal.java

--- BitManipulation/BinaryToDecimal.java
+++ BitManipulation/BinaryToDecimal.java
@@ -18,6 +18,6 @@ public class BinaryToDecimal
             num/=10;
         }
         System.out.println("Result :"+"\n"+ decimalNumber);
-        System.out.println("Hello ");
+        System.out.println("Hello World");
     }
 }
